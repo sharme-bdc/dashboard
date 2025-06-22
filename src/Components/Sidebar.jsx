72cbe8a1ad6cfd6a100e1b13340d185aad460718
sharme-bdc <sharmeakter777@gmail.com>
2025-06-22 09:15:54 -0700

    text

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col min-h-screen py-4 px-8 justify-between text-white w-4/12">
+            <div>
+                <img src='src/Assets/Profile.png' alt='Profile'/>
+                <p className='text-white font-bold text-3xl pt-4'>Samantha</p>
+                <p>samantha@email.com</p>
+            </div>
+            <div>Navbar
+            </div>
         </div>
     );
 };
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Sidebar from '../Components/Sidebar';
 import { Outlet } from 'react-router';
 
 const Layout = () => {
     return(
         <section className='bg-gray-300 p-2 items-center pr-2'>
             <div className='flex flex-col w-full bg-black rounded-lg sm:flex-row py-4 px-2'> 
-                <div className="text-red-500">Sidebar</div>
+                <Sidebar />``
                 <main><Outlet /></main>
             </div>
         </section>
